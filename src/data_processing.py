1169f9f1e7f6e92104e85a64f92f4272c596c09a
eben4ya <benaya.imanuela@gmail.com>
2024-11-20 17:28:10 +0700

    feat: added predictive algorithm

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import pytz
 
 def load_data():
@@ -13,3 +14,21 @@ def filter_data(data, start_date, end_date):
     
     # Filter the data using the timezone-aware timestamps
     return data[(data['datetime_wib'] >= start_date) & (data['datetime_wib'] <= end_date)]
+
+def calculate_regression_coefficients(data):
+    # Define input variables (X) and output variable (Y)
+    X = data[['temperature_c', 'humidity_percent', 'pressure_hpa', 'wind_speed_ms']].values
+    Y = data['aqi'].values
+
+    # Add a bias term (intercept)
+    X = np.c_[np.ones(X.shape[0]), X]
+
+    # Calculate regression coefficients using the normal equation
+    coefficients = np.linalg.inv(X.T @ X) @ X.T @ Y
+    return coefficients
+
+def predict_aqi(coefficients, input_data):
+    # Add a bias term (intercept) to input data
+    input_data = np.c_[np.ones(input_data.shape[0]), input_data]
+    predictions = input_data @ coefficients
+    return predictions
